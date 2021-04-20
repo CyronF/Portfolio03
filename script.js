82b0d5d72ac429996d5186f6c1c761d0345aba2f
CyronF <cyronpfraser@gmail.com>
2021-04-20 18:54:06 -0400

    debugged get user options

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function getUserOptions() {
     'y',
     'z'
   ];
-  var upperCaseCharacters = [
+  var upperCaseCharactersArray = [
     'A',
     'B',
     'C',
@@ -57,9 +57,9 @@ function getUserOptions() {
     'Y',
     'Z'
   ];
-  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  var numericCharactersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-  var specialCharacters = [
+  var specialCharactersArray = [
     '@',
     '%',
     '+',
@@ -85,46 +85,46 @@ function getUserOptions() {
     '.'
   ];
 
-  var lengthRange = [];
-  var userOptions = {
-    lowerCaseCharactersArray: lowerCaseCharactersArray,
-    upperCaseCharacters: upperCaseCharacters,
-    numericCharacters: numericCharacters,
-    specialCharacters: specialCharacters,
-    lengthRange: lengthRange
-  };
-  lengthRange = prompt("How long would you like your password to be")
-  lengthRange = parseInt(lengthRange);
+  var lengthRanges = [];
+  
+  lengthRanges = prompt("How long would you like your password to be")
+  lengthRanges = parseInt(lengthRanges);
   // ask for lowercase
-  lowerCaseCharactersArray = confirm("would you like to use lower case letters in your password?");
+  var lowerCaseUserChoice = confirm("would you like to use lower case letters in your password?");
   // ask for uppercase
-  upperCaseCharacters = confirm("would you like to use upper case letters in your password?");
+  var upperCaseUserChoice = confirm("would you like to use upper case letters in your password?");
   // ask for numeric character
-  numericCharacters = confirm("would you like to use numeric characters in your password?");
+  var numericCharactersUserChoice = confirm("would you like to use numeric characters in your password?");
   // ask for special character
-  specialCharacters = confirm("would you like to use special characters in your password?");
+  var specialCharacterUserChoice = confirm("would you like to use special characters in your password?");
   //prompt for length
 
-
-
-  return userOptions
+  var userOptions = {
+    lowerCaseUserChoice: lowerCaseUserChoice,
+    upperCaseUserChoice: upperCaseUserChoice,
+    numericCharactersUserChoice: numericCharactersUserChoice,
+    specialCharacterUserChoice: specialCharacterUserChoice,
+    lengthRanges: lengthRanges
+  };
+return userOptions
 }
 
-function lengthRange(inputTxt, minLength, maxLength)
+function lengthRanges(inputTxt, minLength, maxLength) {
 var minLength = '8'
 var maxLength = '128'
-
-if (field.minlength < '8' || field.maxlength > '128') {
-  alert("please provide a value between " + minlen + " and " + mxlen + " characters")
-
 }
 
-if (!lowerCaseCharactersArray === false,
-  !upperCaseCharacters === false,
-  !numericCharacters === false,
-  !specialCharacters === false) {
-  alert('please select one special char group lower/upper/special/numeric')
-}
+//if (field.minlength < '8' || field.maxlength > '128') {
+  //alert("please provide a value between " + minlen + " and " + mxlen + " characters")
+
+//}
+
+// if (!lowerCaseCharactersArray === false,
+//   !upperCaseCharactersArray === false,
+//   !numericCharactersArray === false,
+//   !specialCharactersArray === false) {
+//   alert('please select one special char group lower/upper/special/numeric')
+//}
 //build options 
 
 
@@ -138,11 +138,11 @@ function generatePassword() {
   var mustHaveCharacters = [];
   var canHaveCharacters = []
   var userOptions = getUserOptions();
+console.log
 
-
-  if (userOptions.upperCaseCharacters) {
-    canHaveCharacters.concat(upperCaseCharacters)
-    mustHaveCharacters.concat(upperCaseCharacters[getRandomFromLength(upperCaseCharacters)])
+  if (userOptions.upperCaseCharactersArray) {
+    canHaveCharacters.concat(upperCaseCharactersArray)
+    mustHaveCharacters.concat(upperCaseCharactersArray[getRandomFromLength(upperCaseCharactersArray)])
   }
 
   if (userOptions.lowerCaseCharactersArray) {
@@ -150,18 +150,18 @@ function generatePassword() {
     mustHaveCharacters.concat(lowerCaseCharactersArray[getRandomFromLength(lowerCaseCharactersArray)])
   }
 
-  if (userOptions.numericCharacters) {
-    canHaveCharacters.concat(numericCharacters)
-    mustHaveCharacters.concat(numericCharacters[getRandomFromLength(numericCharacters)])
+  if (userOptions.numericCharactersArray) {
+    canHaveCharacters.concat(numericCharactersArray)
+    mustHaveCharacters.concat(numericCharactersArray[getRandomFromLength(numericCharactersArray)])
   }
 
-  if (userOptions.specialCharacters) {
-    canHaveCharacters.concat(specialCharacters)
-    mustHaveCharacters.concat(specialCharacters[getRandomFromLength(specialCharacters)])
+  if (userOptions.specialCharactersArray) {
+    canHaveCharacters.concat(specialCharactersArray)
+    mustHaveCharacters.concat(specialCharactersArray[getRandomFromLength(specialCharactersArray)])
   }
 
   for (let currIndx = 0; currIndx < userOptions.lengthChosen; currIndx++); {
-    passwordArray.push(canHaveCharacters[getRandomFromLength(upperCaseCharacters)]);
+    passwordArray.push(canHaveCharacters[getRandomFromLength(upperCaseCharactersArray)]);
   }
 
   for (let i = 0; i < mustHaveCharacters.length; i++) {
@@ -172,10 +172,10 @@ function generatePassword() {
 }
 
 // Write password to the #password input
-function generatePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
+// function generatePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+//   passwordText.value = password;
+// }
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
