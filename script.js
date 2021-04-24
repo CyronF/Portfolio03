84d1083b1942aaec8256401840d13049d513eb4a
CyronF <cyronpfraser@gmail.com>
2021-04-24 15:29:26 -0400

    fixed syntax error and edited js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- var lowerCaseCharactersArray = [
+var lowerCaseCharactersArray = [
   'a',
   'b',
   'c',
@@ -83,93 +83,103 @@ var specialCharactersArray = [
 ];
 
 
-var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", generatePassword);
-
 function generatePassword() {
- var userOptions = getUserOptions(); 
+  // when i run this function, i expect to get back a string
+  // the string will contain a mix of options
+  var userOptions = getUserOptions();
 
- var passwordArray = [];
+  // combo of musthave and canhave characters
+  var passwordArray = [];
 
- var mustHaveCharacters = [];
+  // give us length of the characters
+  // based on user options (uppercase, lowercase, numbers)
+  var mustHaveCharacters = [];
 
- var canHaveCharacters = [];
+  // probably depends on length chosen
+  // data type: array
+  var canHaveCharacters = [];
 
-function getUserOptions() {
-  var length = Number(prompt("How many characters would you like your password to be?"));
-  while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Password length must be 8-128 characters."))
-  
-  var lowerCaseUserChoice = confirm("would you like to use lower case letters in your password?");
-  
-  var upperCaseUserChoice = confirm("would you like to use upper case letters in your password?");
-  
-  var numericCharactersUserChoice = confirm("would you like to use numeric characters in your password?");
-  
-  var specialCharacterUserChoice = confirm("would you like to use special characters in your password?");
-  
-  var userOptions = {
-    lowerCaseUserChoice: lowerCaseUserChoice,
-    upperCaseUserChoice: upperCaseUserChoice,
-    numericCharactersUserChoice: numericCharactersUserChoice,
-    specialCharacterUserChoice: specialCharacterUserChoice,
-    length: length
+  function getUserOptions() {
+    var length = Number(prompt("How many characters would you like your password to be?"));
+    while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Password length must be 8-128 characters."))
+
+    var lowerCaseUserChoice = confirm("would you like to use lower case letters in your password?");
+
+    var upperCaseUserChoice = confirm("would you like to use upper case letters in your password?");
+
+    var numericCharactersUserChoice = confirm("would you like to use numeric characters in your password?");
+
+    var specialCharacterUserChoice = confirm("would you like to use special characters in your password?");
+
+    
+
+    var userOptions = {
+      lowerCaseUserChoice: lowerCaseUserChoice,
+      upperCaseUserChoice: upperCaseUserChoice,
+      numericCharactersUserChoice: numericCharactersUserChoice,
+      specialCharacterUserChoice: specialCharacterUserChoice,
+      length: length
+    }
+
+    return userOptions;
   }
 
-return userOptions;
-}
-
-function getRandomFromLength(arr) {
-  return Math.floor(Math.random() *arr.length)
+  function getRandomFromLength(arr) {
+    return Math.floor(Math.random() * arr.length)
   }
 
   if (userOptions.upperCaseUserChoice) {
-    canHaveCharacters.concat(upperCaseCharactersArray)
-    mustHaveCharacters.concat(upperCaseCharactersArray[getRandomFromLength(upperCaseCharactersArray)])
+    canHaveCharacters = canHaveCharacters.concat(upperCaseCharactersArray[getRandomFromLength(upperCaseCharactersArray)])
+    mustHaveCharacters = mustHaveCharacters.concat(upperCaseCharactersArray[getRandomFromLength(upperCaseCharactersArray)])
   }
 
   if (userOptions.lowerCaseUserChoice) {
-    canHaveCharacters.concat(lowerCaseCharactersArray)
-    mustHaveCharacters.concat(lowerCaseCharactersArray[getRandomFromLength(lowerCaseCharactersArray)])
+   canHaveCharacters = canHaveCharacters.concat(lowerCaseCharactersArray)
+    mustHaveCharacters = mustHaveCharacters.concat(lowerCaseCharactersArray[getRandomFromLength(lowerCaseCharactersArray)])
   }
 
   if (userOptions.numericCharactersUserChoice) {
-    canHaveCharacters.concat(numericCharactersArray)
-    mustHaveCharacters.concat(numericCharactersArray[getRandomFromLength(numericCharactersArray)])
+    canHaveCharacters = canHaveCharacters.concat(numericCharactersArray)
+    mustHaveCharacters = mustHaveCharacters.concat(numericCharactersArray[getRandomFromLength(numericCharactersArray)])
   }
-  
+
   if (userOptions.specialCharacterUserChoice) {
-    canHaveCharacters.concat(specialCharactersArray)
-    mustHaveCharacters.concat(specialCharactersArray[getRandomFromLength(specialCharactersArray)])
+   canHaveCharacters = canHaveCharacters.concat(specialCharactersArray)
+    mustHaveCharacters = mustHaveCharacters.concat(specialCharactersArray[getRandomFromLength(specialCharactersArray)])
   }
 
   if (!lowerCaseCharactersArray === false,
-   !upperCaseCharactersArray === false,
-   !numericCharactersArray === false,
-   !specialCharactersArray === false) {
-     alert('please select one special char group lower/upper/special/numeric')
-   }
-   
-  for (let currIndx = 0; currIndx < userOptions.length; currIndx++); {
+    !upperCaseCharactersArray === false,
+    !numericCharactersArray === false,
+    !specialCharactersArray === false) {
+    alert('please select one special char group lower/upper/special/numeric')
+  }
+
+
+  for (let i = 0; i < +userOptions.length; i++) {
     passwordArray.push(canHaveCharacters[getRandomFromLength(upperCaseCharactersArray)]);
-   }
-   
-  for (let i = 0; i < mustHaveCharacters.length; i++) {
-     password[i] = mustHaveCharacters[i];
-  } 
   
+  }
+
+  for (let i = 0; i < mustHaveCharacters.length; i++) {
+    passwordArray[i] = mustHaveCharacters[i];
+  }
+  
+return passwordArray;
 }
+  var generateBtn = document.querySelector("#generate");
+      generateBtn.addEventListener("click", writePassword);
 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-return password;
-
 }
 
-generateBtn.addEventListener("click", writePassword);
 
 
-  
+
+
+
+
